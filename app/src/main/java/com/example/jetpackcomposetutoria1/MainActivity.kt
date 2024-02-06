e12d23b688027b91100e6ba2a2969b2b85d936be
Shelton17 <okangajeff17@gmail.com>
2024-02-06 12:44:32 +0300

    Update: creating a gallery view in jetpack compose with image cards, rows and columns

--- app/src/main/java/com/example/jetpackcomposetutoria1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutoria1/MainActivity.kt
@@ -1,41 +1,116 @@
 package com.example.jetpackcomposetutoria1
 
+import android.accounts.AuthenticatorDescription
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutoria1.ui.theme.JetPackComposeTutoria1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column (
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(400.dp)
-                    .background(Color.Green),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("Hello")
-                Text("World")
+            val painter = painterResource(id = R.drawable.sarah_cabin_crew)
+            val description = "Cabin Crew"
+            val title = "Cabin Crew"
+            Column {
+                Row (verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End){
+                    Box(modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .padding(16.dp)){
+                        ImageCard(painter = painterResource(id = R.drawable.sarah_pink_chair), contentDescription = "Birthday", title = "Birthday")
+                    }
+                    Box(modifier = Modifier
+                        .padding(16.dp)){
+                        ImageCard(painter = painter, contentDescription = description, title = title)
+                    }
+                }
+                Row (verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End){
+                    Box(modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .padding(16.dp)){
+                        ImageCard(painter = painter, contentDescription = description, title = title)
+                    }
+                    Box(modifier = Modifier
+                        .padding(16.dp)){
+                        ImageCard(painter = painter, contentDescription = description, title = title)
+                    }
+                }
             }
 
+
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+){
+    Card (
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(25.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+    ){
+        Box(modifier = Modifier.height(200.dp)){
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop)
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 200f
+                    )
+                ))
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart){
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
         }
     }
 }
